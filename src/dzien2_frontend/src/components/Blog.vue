320fb8c6c56aa7623f08968c5cab21a7e2e3e7b4
Sttag <wiktor.pietraszuk@gmail.com>
2024-06-26 19:08:18 +0200

    Added styles

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,38 +1,39 @@
 <template>
-  <div>
-    <h2 class="text-blue-600">Wpisy na bloga:</h2>
-      <button @click="pobierzWpisy">refresh</button>
-      <div v-for="wpis in wpisy">
-            <p>  {{ wpisy }}</p>
-      </div>
-
-    
-      <input v-model="nowyBlog" type="text">
-      <button @click="dodajWpisy">dodaj</button>
-  </div>
+    <div>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4" >
+        <div v-for="wpis in wpisy" class=" drop-shadow-xl bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+        </div>
+    </div class="flex justify-center">
+        <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+        <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4" >dodaj</button>
+    </div>
 </template>
 
 <script>
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
 
-export default {
-  data() {
-      return {
-          wpisy: [],
-          nowyBlog: ""
-      }
-  },
-  methods: {
-      async dodajWpisy() {
-          await dzien2_backend.dodaj_wpis(this.nowyBlog);
-      },
-      async pobierzWpisy() {
-          this.wpisy = await dzien2_backend.odczytaj_wpisy();
-      }
-  },
-  async mounted(){
-    this.pobierzWpisy()
-  }
-
+export default{
+    data(){
+        return{
+            wpisy:[],
+            nowyBlog: ""
+        }
+    },
+    methods:{
+        async dodajWpisy(){
+            this.wpisy = await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy(){
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
